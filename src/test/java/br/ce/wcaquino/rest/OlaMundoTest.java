063afc7372e094d7774e16ec3f2581f164227d91
Henrique Cervi <henrique.cervi@terra.com.br>
2021-06-17 16:35:10 -0300

    Aprendendo sobre Matchers do Hamcrest

--- src/test/java/br/ce/wcaquino/rest/OlaMundoTest.java
+++ src/test/java/br/ce/wcaquino/rest/OlaMundoTest.java
@@ -4,21 +4,25 @@ import io.restassured.RestAssured;
 import io.restassured.http.Method;
 import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
-import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
 
 public class OlaMundoTest {
 
     @Test
     public void testOlaMundo() {
         Response response = RestAssured.request(Method.GET, "https://restapi.wcaquino.me/ola");
-        Assert.assertTrue(response.getBody().asString().equals("Ola Mundo!"));
-        Assert.assertTrue(response.statusCode() == 200);
-        Assert.assertTrue("O Status Code deve ser 200", response.statusCode() == 200);
-        Assert.assertEquals(200, response.statusCode());
+        assertTrue(response.getBody().asString().equals("Ola Mundo!"));
+        assertTrue(response.statusCode() == 200);
+        assertTrue("O Status Code deve ser 200", response.statusCode() == 200);
+        assertEquals(200, response.statusCode());
     }
 
     @Test
@@ -34,5 +38,27 @@ public class OlaMundoTest {
                 .get("https://restapi.wcaquino.me/ola")
         .then() //Assertiva
                 .statusCode(200);
+    }
+
+    @Test
+    public void devoConhecerMatchersHamcrest() {
+        assertThat("Maria", is("Maria"));
+        assertThat(128, is(128));
+        assertThat(128, isA(Integer.class));
+        assertThat(128d, isA(Double.class));
+        assertThat(128d, greaterThan(120d));
+        assertThat(128d, lessThan(135d));
+
+        List<Integer> impares = Arrays.asList(1,3,5,7,9);
+        assertThat(impares, hasSize(5));
+        assertThat(impares, contains(1,3,5,7,9));
+        assertThat(impares, containsInAnyOrder(1,5,9,7,3));
+        assertThat(impares, hasItem(1));
+        assertThat(impares, hasItems(1,5));
+
+        assertThat("Maria", is(not("João")));
+        assertThat("Maria", not("João"));
+        assertThat("Joaquina", anyOf(is("Maria"), is("Joaquina")));
+        assertThat("Joaquina", allOf(startsWith("Joa"), endsWith("ina"), containsString("qui")));
     }
 }
